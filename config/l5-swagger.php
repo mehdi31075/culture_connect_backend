efe5da0c09f758ca91dbfa89623462241d908bd4
mehdi31075 <mehdi031075@yahoo.com>
2025-10-08 01:36:13 +0330

    Update L5 Swagger configuration: modify the host constant to use APP_URL as a fallback for improved environment flexibility.

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -318,7 +318,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://localhost:8000'),
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', env('APP_URL', 'http://localhost:8000')),
         ],
     ],
 ];
